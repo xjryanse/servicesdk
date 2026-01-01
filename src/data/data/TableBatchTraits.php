b80d233b5a5a6b2db2746ca6bbe27add31cd21f7
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-01-01 21:16:56 +0800

    Update TableBatchTraits.php

--- src/data/data/TableBatchTraits.php
+++ src/data/data/TableBatchTraits.php
@@ -7,6 +7,28 @@ use xjryanse\servicesdk\msgq\QLogSdk;
  * 缓存类
  */
 trait TableBatchTraits{
+    
+    /**
+     * 取单挑数据
+     * @param type $msgId   消息id
+     * @param type $type    消息类型
+     * @param type $param   参数
+     */
+    public static function tableBatchDataGet($tableName,$ids){
+        $url = static::sdkUrl('data/tableBatch/get');
+        // 默认发本地消息中间件
+        // TODO:配置解耦
+        $data['table_name'] = $tableName;
+        $data['id']         = $ids;
+dump($url);
+dump($data);
+
+
+
+        $res                = QLogSdk::postAndLog($url, $data);
+        return $res['data'];
+    }
+    
     /**
      * 
      * @param type $tableName
