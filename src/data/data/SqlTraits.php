73a6fcc9fc49f7b51983e1c15de666e383e62cd5
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-01-02 21:05:27 +0800

    Update SqlTraits.php

--- src/data/data/SqlTraits.php
+++ src/data/data/SqlTraits.php
@@ -34,4 +34,23 @@ trait SqlTraits{
         return $res['data'];
     }
 
+    /**
+     * 20260102
+     * @param type $sqlParam
+     * @return type
+     * @throws Exception
+     */
+    public static function rawSqlQuery($sqlParam){
+        $url = static::sdkUrl('data/sql/rawSqlQuery');
+        
+        $param                   = [];
+        $param['sqlParam']         = $sqlParam;
+
+        $res                    = QLogSdk::postAndLog($url, $param);
+        if(!$res){
+            throw new Exception('没有获取到接口数据:'.$url);
+        }
+        return $res['data'];
+    }
+
 }
