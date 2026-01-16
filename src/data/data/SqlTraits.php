80485cf897b09f5d02ba87e18e8a9691caab3684
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-01-16 23:26:21 +0800

    Update SqlTraits.php

--- src/data/data/SqlTraits.php
+++ src/data/data/SqlTraits.php
@@ -3,6 +3,7 @@ namespace xjryanse\servicesdk\data\data;
 
 use xjryanse\speedy\logic\Url;
 use xjryanse\servicesdk\msgq\QLogSdk;
+use xjryanse\servicesdk\msgq\WQLogSdk;
 use Exception;
 
 /**
@@ -20,14 +21,17 @@ trait SqlTraits{
     public static function sqlDataPaginate($sqlKey, $orderBy='', $param=[]){
         $url = static::sdkUrl('data/sql/paginate');
         
-        $getP                   = [];
-        $getP['sqlKey']         = $sqlKey;
+        $param['sqlKey']        = $sqlKey;
         if($orderBy){
-            $getP['orderBy']    = $orderBy;
+            $param['orderBy']    = $orderBy;
         }
-        $urlFinal               = Url::addParam($url, $getP);
 
-        $res                    = QLogSdk::postAndLog($urlFinal, $param);
+        $baseUrl = 'data/sql/paginate';
+        // 默认发本地消息中间件
+        // TODO:配置解耦
+        $host = static::workerIp();
+        $port = static::workerPort();
+        $res = WQLogSdk::request($host, $port, $baseUrl, $param);        
         if(!$res){
             throw new Exception('没有获取到接口数据:'.$url);
         }
