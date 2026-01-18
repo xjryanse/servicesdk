fd1abf7ddc2dae5909b69f000869924fd8b05555
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-01-18 23:11:06 +0800

    1

--- src/export/ExportSdk.php
+++ src/export/ExportSdk.php
@@ -18,25 +18,41 @@ class ExportSdk {
     protected static function workerPort(){
         return '19904';
     }
+
     /**
-     * 同步导出excel
+     * 同步导出 excel
      * @param type $msgId   消息id
      * @param type $type    消息类型
      * @param type $param   参数
      */
-    public static function excelSync($exportData, $dataTitle){
-        $url = 'http://10.9.0.202:9924/export/excel/sync';
+    public static function excelSync ($exportData, $dataTitle) {
+        $url = 'http://10.9.0.2:9924/export/excel/sync';
         // 默认发本地消息中间件
         // TODO:配置解耦
         $data['exportData']  = $exportData;
         $data['dataTitle']   = $dataTitle;
-        
-// dump(json_encode($data,JSON_UNESCAPED_UNICODE));exit;
-// dump(json_encode($dataTitle,JSON_UNESCAPED_UNICODE));
         $res                 = Query::posturl($url, $data);
 
         return $res['data'];
     }
+
+    /**
+     * 同步导出 csv 文件
+     * @param type $exportData
+     * @param type $dataTitle
+     * @return type
+     */
+    public static function csvSync ($exportData, $dataTitle) {
+        $url = 'http://10.9.0.2:9924/export/csv/sync';
+        // 默认发本地消息中间件
+        // TODO:配置解耦
+        $data['exportData']  = $exportData;
+        $data['dataTitle']   = $dataTitle;
+        $res                 = Query::posturl($url, $data);
+
+        return $res['data'];
+    }
+    
     
 
 }
