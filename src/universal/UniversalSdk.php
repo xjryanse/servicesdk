b52fc97c417a9e64e927f27ef5ced906c47c8e65
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-01-24 16:18:41 +0800

    Update UniversalSdk.php

--- src/universal/UniversalSdk.php
+++ src/universal/UniversalSdk.php
@@ -2,7 +2,7 @@
 namespace xjryanse\servicesdk\universal;
 
 use xjryanse\servicesdk\comm\SdkBase;
-use xjryanse\phplite\facade\Cache;
+use xjryanse\phplite\cache\PCache;
 use xjryanse\servicesdk\msgq\QLogSdk;
 /**
  * 
@@ -17,9 +17,9 @@ class UniversalSdk extends SdkBase{
      * @param type $type    消息类型
      * @param type $param   参数
      */
-    public static function tableDynArrs($pageItemId){
+    public function tableDynArrs($pageItemId){
         $key = __CLASS__.__METHOD__.$pageItemId;
-        return Cache::funcGet($key,function () use ($pageItemId) {
+        return PCache::funcGet($key,function () use ($pageItemId) {
             $url = static::sdkUrl('universal/table/dynArrs');
             // 默认发本地消息中间件
             // TODO:配置解耦
@@ -33,9 +33,9 @@ class UniversalSdk extends SdkBase{
      * @param type $pageKey
      * @return type
      */
-    public static function pageConfig($pageKey){
+    public function pageConfig($pageKey){
         $key = __CLASS__.__METHOD__.$pageKey;
-        return Cache::funcGet($key,function () use ($pageKey) {
+        return PCache::funcGet($key,function () use ($pageKey) {
             $url = static::sdkUrl('universal/page/config');
             // 默认发本地消息中间件
             // TODO:配置解耦
@@ -44,5 +44,21 @@ class UniversalSdk extends SdkBase{
             return $res['data'];
         });
     }
-    
+    /**
+     * 2026年1月23日
+     * @param type $pageKey
+     * @return type
+     */
+    public function pageVue($pageKey){
+        $key = __CLASS__.__METHOD__.$pageKey;
+        PCache::rm($key);
+        return PCache::funcGet($key,function () use ($pageKey) {
+            $url = static::sdkUrl('universal/page/vue');
+            // 默认发本地消息中间件
+            // TODO:配置解耦
+            $data['pageKey']    = $pageKey;
+            $res                = QLogSdk::postAndLog($url, $data);
+            return $res['data'];
+        });
+    }
 }
