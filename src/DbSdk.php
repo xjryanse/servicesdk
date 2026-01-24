589a2cf8abf9a3b4ef4700da70d80633b3456e77
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-01-24 15:23:37 +0800

    Update DbSdk.php

--- src/DbSdk.php
+++ src/DbSdk.php
@@ -14,19 +14,18 @@ class DbSdk {
      * @param type $dbCate
      * @return type
      */
-    public static function dbId($dbCate){
+    public static function dbId($dbCate, $hostBindId = ''){
         if($dbCate == 'dbEntry'){
             return 0;
         }
-        $serverPort = $_SERVER['SERVER_PORT'];
-        if($serverPort >= 9900){
-            // 微服务
-            throw new Exception('微服务暂不适用本方法');
+        if($hostBindId){
+            // 透传id直接取
+            $confArr    = EntrySdk::bindIdInfo($hostBindId);
         } else {
-            // 客户站点
+            // 没有的用域名取
             $httpHost   = $_SERVER['HTTP_HOST'];
+            $confArr    = EntrySdk::hostBindInfo($httpHost);
         }
-        $confArr    = EntrySdk::hostBindInfo($httpHost);
         // 业务库配置
         $dbInfo     = Arrays::value($confArr, $dbCate);
         return $dbInfo['id'];
