f1879806668f78edef122f26620dc0310bb9adf2
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-01-26 23:08:15 +0800

    1

--- src/universal/UniversalSdk.php
+++ src/universal/UniversalSdk.php
@@ -11,6 +11,8 @@ class UniversalSdk extends SdkBase{
     // 需定义：配套BindSdkTrait使用
     protected static $serverKey = 'service_universal';
 
+    use \xjryanse\servicesdk\universal\universal\PageTraits;
+    
     /**
      * 取单挑数据
      * @param type $msgId   消息id
@@ -28,60 +30,5 @@ class UniversalSdk extends SdkBase{
             return $res['data'];
         });
     }
-    /**
-     * 2026年1月23日
-     * @param type $pageKey
-     * @return type
-     */
-    public function pageConfig($pageKey){
-        $key = __CLASS__.__METHOD__.$pageKey;
-        PCache::rm($key);        
-        return PCache::funcGet($key,function () use ($pageKey) {
-            $url = static::sdkUrl('universal/page/config');
-            // 默认发本地消息中间件
-            // TODO:配置解耦
-            $data['pageKey']    = $pageKey;
-            $data['svBindId']   = $this->uuid;
-            
-            $res                = QLogSdk::postAndLog($url, $data);
-            return $res['data'];
-        });
-    }
-    /**
-     * 2026年1月23日
-     * @param type $pageKey
-     * @return type
-     */
-    public function pageVue($pageKey){
-        $key = __CLASS__.__METHOD__.$pageKey;
-        PCache::rm($key);
-        return PCache::funcGet($key,function () use ($pageKey) {
-            $url = static::sdkUrl('universal/page/vue');
-            // 默认发本地消息中间件
-            // TODO:配置解耦
-            $data['pageKey']    = $pageKey;
-            $data['svBindId']   = $this->uuid;
-            $res                = QLogSdk::postAndLog($url, $data);
-            return $res['data'];
-        });
-    }
-    
-    /**
-     * 2026年1月23日:注意这里是id不是key(历史问题)
-     * @param type $pageId
-     * @return type
-     */
-    public function pageItemVue($pageId){
-        $key = __CLASS__.__METHOD__.$pageId;
-        PCache::rm($key);
-        return PCache::funcGet($key,function () use ($pageId) {
-            $url = static::sdkUrl('universal/page/itemVue');
-            // 默认发本地消息中间件
-            // TODO:配置解耦
-            $data['id']         = $pageId;
-            $data['svBindId']   = $this->uuid;
-            $res                = QLogSdk::postAndLog($url, $data);
-            return $res['data'];
-        });
-    }
+
 }
---
+++ src/universal/universal/PageTraits.php
@@ -0,0 +1,84 @@
+<?php
+namespace xjryanse\servicesdk\universal\universal;
+
+use xjryanse\phplite\cache\PCache;
+use xjryanse\servicesdk\msgq\QLogSdk;
+/**
+ * 
+ */
+trait PageTraits{
+    /**
+     * 2026年1月23日
+     * @param type $pageKey
+     * @return type
+     */
+    public function pageConfig($pageKey){
+        $key = __CLASS__.__METHOD__.$pageKey;
+        PCache::rm($key);        
+        return PCache::funcGet($key,function () use ($pageKey) {
+            $url = static::sdkUrl('universal/page/config');
+            // 默认发本地消息中间件
+            // TODO:配置解耦
+            $data['pageKey']    = $pageKey;
+            $data['svBindId']   = $this->uuid;
+            
+            $res                = QLogSdk::postAndLog($url, $data);
+            return $res['data'];
+        });
+    }
+    /**
+     * 2026年1月23日
+     * @param type $pageKey
+     * @return type
+     */
+    public function pageVue($pageKey){
+        $key = __CLASS__.__METHOD__.$pageKey;
+        PCache::rm($key);
+        return PCache::funcGet($key,function () use ($pageKey) {
+            $url = static::sdkUrl('universal/page/vue');
+            // 默认发本地消息中间件
+            // TODO:配置解耦
+            $data['pageKey']    = $pageKey;
+            $data['svBindId']   = $this->uuid;
+            $res                = QLogSdk::postAndLog($url, $data);
+            return $res['data'];
+        });
+    }
+    
+    /**
+     * 2026年1月23日
+     * @param type $pageKey
+     * @return type
+     */
+    public function pageItemVue($pageKey){
+        $key = __CLASS__.__METHOD__.$pageKey;
+        PCache::rm($key);
+        return PCache::funcGet($key,function () use ($pageKey) {
+            $url = static::sdkUrl('universal/page/itemVue');
+            // 默认发本地消息中间件
+            // TODO:配置解耦
+            $data['pageKey']    = $pageKey;
+            $data['svBindId']   = $this->uuid;
+            $res                = QLogSdk::postAndLog($url, $data);
+            return $res['data'];
+        });
+    }
+    /**
+     * 2026年1月26日
+     * @param type $pageKey
+     * @return type
+     */
+    public function pageKeyObj(){
+        $key = __CLASS__.__METHOD__;
+        PCache::rm($key);
+        return PCache::funcGet($key,function () {
+            $url = static::sdkUrl('universal/page/keyObj');
+            // 默认发本地消息中间件
+            // TODO:配置解耦
+            // $data['pageKey']    = $pageKey;
+            $data['svBindId']   = $this->uuid;
+            $res                = QLogSdk::postAndLog($url, $data);
+            return $res['data'];
+        });
+    }
+}
