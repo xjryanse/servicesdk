7e4a170c893eb992e9bc9cf86003ee62ea0b44d4
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-02-01 19:31:25 +0800

    Update BindSdkTrait.php

--- src/comm/traits/BindSdkTrait.php
+++ src/comm/traits/BindSdkTrait.php
@@ -30,7 +30,7 @@ trait BindSdkTrait{
             $list       = EntrySdk::serverList($bindId, $serverKey);
             if(!$list){
                 $info = EntrySdk::bindIdInfo($bindId);
-                throw new Exception('绑定号'.$bindId.'对应的域名'.$info['host'].'没有配置'.$serverKey.'服务的参数，请联系运维配置');
+                throw new Exception('绑定号'.$bindId.'对应的域名'.$info['host'].'没有配置'.$serverKey.'服务的参数,cacheKey:bindIdInfo');
             }
             // 随机从多个数组中取一条记录
             $this->serverInfo = $list[array_rand($list)];
