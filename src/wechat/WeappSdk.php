aead8bd28e476bf59c89b6ea778df17a913ac61d
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-02-02 16:36:46 +0800

    Create WeappSdk.php

---
+++ src/wechat/WeappSdk.php
@@ -0,0 +1,25 @@
+<?php
+namespace xjryanse\servicesdk\wechat;
+
+use xjryanse\servicesdk\comm\SdkBase;
+
+/**
+ * 微信小程序接入sdk
+ */
+class WeappSdk extends SdkBase{
+    // 需定义：配套BindSdkTrait使用
+    protected static $serverKey = 'service_wechat';
+   /**
+     * 取单挑数据
+     * @param type $msgId   消息id
+     * @param type $type    消息类型
+     * @param type $param   参数
+     */
+    public function getComKeyByAppId($appid){
+        $baseUrl        = 'wechat/company/getComKey';
+        $data['appid']  = $appid;
+        $res            = $this->queryLog($baseUrl, $data, 'curl');
+        return $res['data'];        
+    }
+    
+}
