de7a45de6c034412179586b5b16a6b8acf669d18
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-03-12 09:48:51 +0800

    1

--- src/comm/traits/BindSdkTrait.php
+++ src/comm/traits/BindSdkTrait.php
@@ -30,13 +30,13 @@ trait BindSdkTrait{
             }
             $serverKey  = static::$serverKey;
             $list       = EntrySdk::serverList($bindId, $serverKey);
-            if(!$list){
+            if(!$list || !is_array($list)){
                 $info = EntrySdk::bindIdInfo($bindId);
                 $host = Arrays::value($info,'host');
                 EntrySdk::clearCache('bindIdInfo', $bindId);
                 throw new Exception('绑定号'.$bindId.'对应的域名'.$host.'没有配置'.$serverKey.'服务的参数，请联系运维配置');
             }
-            // 随机从多个数组中取一条记录
+            // 随机从多个数组中取一条记录（防 serverList 返回非数组导致 array_rand 报错）
             $this->serverInfo = $list[array_rand($list)];
         }
         return $this->serverInfo;
