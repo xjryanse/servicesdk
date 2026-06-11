5fabb241c06e24f45d492d0a6888c5dd83b6c47c
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-06-11 14:29:16 +0800

    Update MessageSdk.php

--- src/message/MessageSdk.php
+++ src/message/MessageSdk.php
@@ -19,10 +19,19 @@ class MessageSdk extends SdkBase
      * @param string $channel curl|worker，默认 curl
      * @return mixed taskIds、tasks 等，见中台接口 data
      */
-    public function submit(array $param, $channel = 'curl')
+    public function submit($dataId, $msgTplCode, array $param, $msgChannel = [], $channel = 'curl')
     {
+        $qData = [
+            'bizId'         => $dataId,
+            'tplCode'       => $msgTplCode,
+            'rawData'       => $param,
+            'channels'      => $msgChannel,  // 可选，不传则用模板已绑定渠道
+            // 防止同一条业务消息被重复提交、重复推送的唯一键
+            // 'idempotentKey' => 'warehouse_out_20260611001:WAREHOUSE_OUT_NOTICE:webhook',  // 可选
+        ];
+        
         $baseUrl = 'message/task/submit';
-        $data = array_merge($this->postBaseData(), $param);
+        $data = array_merge($this->postBaseData(), $qData);
         $res = $this->queryLog($baseUrl, $data, $channel);
         return $res['data'];
     }
