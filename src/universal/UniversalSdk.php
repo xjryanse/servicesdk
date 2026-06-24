702aaed38a55cf02f170c9250dcb4d9147b8cc56
xieJialun <36151536+xjryanse@users.noreply.github.com>
2026-06-24 18:12:55 +0800

    1

--- src/universal/UniversalSdk.php
+++ src/universal/UniversalSdk.php
@@ -36,20 +36,6 @@ class UniversalSdk extends SdkBase{
     }
 
     /**
-     * 整页 dynenum 配置（PreData / rawBack 等单条数据场景）。
-     */
-    public function pageDynArrs($pageId){
-        $key = __CLASS__.__METHOD__.$pageId;
-        return PCache::funcGet($key,function () use ($pageId) {
-            $data = $this->postBaseData();
-            $data['page_id'] = $pageId;
-            $data['pageId'] = $pageId;
-            $res = $this->queryLog('universal/page/dynArrs', $data, 'curl');
-            return $res['data'];
-        });
-    }
-
-    /**
      * 2026年6月：表单字段单条配置（uniDynSearch 等）
      */
     public function formGet($fieldId){
@@ -82,6 +68,38 @@ class UniversalSdk extends SdkBase{
             $data['id'] = $pageItemId;
             $data['pageItemId'] = $pageItemId;
             $res = $this->queryLog('universal/pageItem/get', $data, 'curl');
+
+            return $res['data'];
+        });
+    }
+
+    /**
+     * page_item 完整配置（含 optionArr，对标 UniversalPageItemService::get）
+     */
+    public function pageItemInfo($pageItemId)
+    {
+        $key = __CLASS__ . __METHOD__ . $this->uuid . $pageItemId;
+        return PCache::funcGet($key, function () use ($pageItemId) {
+            $data = $this->postBaseData();
+            $data['id'] = $pageItemId;
+            $data['pageItemId'] = $pageItemId;
+            $res = $this->queryLog('universal/pageItem/info', $data, 'curl');
+
+            return $res['data'];
+        });
+    }
+
+    /**
+     * 表格列 optionArr（对标 UniversalItemTableService::optionArr）
+     */
+    public function tableOptionArr($pageItemId)
+    {
+        $key = __CLASS__ . __METHOD__ . $this->uuid . $pageItemId;
+        return PCache::funcGet($key, function () use ($pageItemId) {
+            $data = $this->postBaseData();
+            $data['page_item_id'] = $pageItemId;
+            $data['pageItemId'] = $pageItemId;
+            $res = $this->queryLog('universal/pageItem/tableDynArrs', $data, 'curl');
 
             return $res['data'];
         });
